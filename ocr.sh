7c364ece0d02d08449a8756d1e37213751f31fec
David Eads <davideads@gmail.com>
2015-10-29 00:07:55 -0400

    still fighting

--- ocr.sh
+++ ocr.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-output_file="pages/`basename $1`.txt"
-output_path="pages/`basename $1`"
+echo "Processing $1"
+base_filename=`basename $1 .tif`
+echo $base_filename
+#processed_file="processed/`basename $1`"
+output_file="text/${base_filename}.txt"
+output_path="text/${base_filename}"
+
 if [ ! -e "$output_file" ]; then
-    echo "Processing $1"
-    tesseract -l spa $1 $output_path
+    echo "Preprocessing $1"
+    convert $1 \
+      -rotate "-1.1" \
+      -deskew "-10" \
+      -level 3%,60%,.9 \
+      -despeckle \
+      -noise 2 \
+      +repage \
+      "processed/${base_filename}.tif"
+      #$processed_file
+
+    #unpaper --overwrite --no-deskew --no-grayfilter --no-border-align --no-noisefilter "processed/${base_filename}.pbm" "processed/${base_filename}-unpaper.pbm"
+
+    #convert "processed/${base_filename}-unpaper.pbm" "processed/${base_filename}-unpaper.tif"
+
+    echo "OCR $1"
+    tesseract -l spa "processed/${base_filename}.tif" $output_path
 else
     echo "Skipping $1"
 fi
--- split.sh
+++ split.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gs -o tiff/tomo-001-page-%04d.tif -sDEVICE=tiffgray -r600x600 pdf/tomo-001.pdf
+#gs -o tiff/tomo-001-page-%04d.tif -sDEVICE=tiffgray -r600x600 pdf/tomo-001.pdf
+gs -o tiff/tomo-016-page-%04d.tif -sDEVICE=tiffgray -r300x300 pdf/tomo-016.pdf
